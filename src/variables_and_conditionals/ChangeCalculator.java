ac83675bdf1582c442ee0b0204604c7377e9a92f
Jesus Abarca <jesus.abarca@gompersstudent.org>
2017-11-13 14:24:52 -0800

    Change Calculator Magic 8 Ball

--- src/variables_and_conditionals/ChangeCalculator.java
+++ src/variables_and_conditionals/ChangeCalculator.java
@@ -17,16 +17,21 @@ public class ChangeCalculator {
     public static void main(String[] args) {
 
         // Ask the user how many nickels they have
-
+String nickels = JOptionPane.showInputDialog("How many Nickles do you have?");
         // Convert their answer to an int using Integer.parseInt()
 
+ int num1 = Integer.parseInt(nickels);
         // Ask the user how many dimes they have, and convert their answer
-
+String dimes = JOptionPane.showInputDialog("How many Dimes do you have?");
+int num2 = Integer.parseInt(dimes);
         // Ask the user how many quarters they have, and convert their answer
+String quarters = JOptionPane.showInputDialog("How many Quarters do you have?");
+int num3 = Integer.parseInt(quarters);
 
-        // Calculate how much money the user has and save it in a double variable 
-
+// Calculate how much money the user has and save it in a double variable 
+double money = (num1*.05)+(num2*.10)+(num3*.25); 
         // Tell the user how much money they have
+JOptionPane.showMessageDialog(null, "you have  "+ money);
 
     }
 }
--- src/variables_and_conditionals/Magic8Ball.java
+++ src/variables_and_conditionals/Magic8Ball.java
@@ -2,30 +2,41 @@ package variables_and_conditionals;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
     // 1. Make a main method that includes all the steps below….
+public static void main(String[] args) {
+	int ran = new Random().nextInt(4);
 
     // 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
     // 3. Print out this variable
-
+System.out.println(ran);
     // 4. Get the user to enter a question for the 8 ball
-
-    // 5. If the random number is 0
-
-    // -- tell the user "Yes"
+JOptionPane.showInputDialog("Ask a Question");
 
     // 6. If the random number is 1
-
+if (ran == 1 ) { 
+	JOptionPane.showMessageDialog(null, "No");
+	
+}
     // -- tell the user "No"
 
     // 7. If the random number is 2
-
+else if (ran == 2) {
+	JOptionPane.showMessageDialog(null, "Maybe you should ask google?");
+}
     // -- tell the user "Maybe you should ask Google?"
 
     // 8. If the random number is 3
-
+else if (ran == 3 ) {
+	JOptionPane.showMessageDialog(null, "sure?");
+}
     // -- write your own answer
-
+else {
+	JOptionPane.showMessageDialog(null, "Yes");
+}
+}
 }
